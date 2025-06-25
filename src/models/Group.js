b9e19a495fc3e6452d5f1bc8621d0cdbb5da717c
Gayatri Paraskar <gayatriparaskar133@gmail.com>
2025-06-25 17:38:00 +0530

    sdfdsc

--- src/models/Group.js
+++ src/models/Group.js
@@ -4,7 +4,8 @@ const GroupSchema = new mongoose.Schema({
   name: { type: String, required: true },
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Auth', required: true }],
   admins: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Auth', required: true }], // ✅ list of admin userIds
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  timestamps:true
 });
 
  const GroupModel = mongoose.model('Group', GroupSchema);
