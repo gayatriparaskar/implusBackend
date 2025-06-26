e5b12379ab09b37526c1d3b2341244202e3c76dd
Gayatri Paraskar <gayatriparaskar133@gmail.com>
2025-06-26 12:00:02 +0530

    some changes in chatcontroller for group chat unread

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -200,7 +200,7 @@ module.exports.getchatList = async (req, res) => {
           ],
         },
         {
-          $push: {
+          $addToSet: {
             seenBy: {
               userId: new ObjectId(userId),
               timestamp: new Date(),
@@ -291,21 +291,25 @@ module.exports.getchatList = async (req, res) => {
             };
           }
         }
-        const unreadCount = await GroupChat.countDocuments({
-          groupId: group._id,
-          $or: [
-            { seenBy: { $exists: false } }, // no seenBy at all
-            {
-              seenBy: {
-                $not: {
-                  $elemMatch: {
-                    userId: new ObjectId(userId),
+
+        const unreadCount =
+          markRead === true
+            ? 0
+            : await GroupChat.countDocuments({
+                groupId: group._id,
+                $or: [
+                  { seenBy: { $exists: false } },
+                  {
+                    seenBy: {
+                      $not: {
+                        $elemMatch: {
+                          userId: new ObjectId(userId),
+                        },
+                      },
+                    },
                   },
-                },
-              },
-            },
-          ],
-        });
+                ],
+              });
 
         let lastMsgRead = true;
         if (
