9eb43735fdc234210833b163dd4c246cc1dcee82
Gayatri Paraskar <gayatriparaskar133@gmail.com>
2025-06-26 16:34:13 +0530

    dfdsdsvd

--- src/routes/chatRouter.js
+++ src/routes/chatRouter.js
@@ -11,7 +11,7 @@ chatRouter.get("/messages/:user1/:user2",getMessages);
 chatRouter.get("/full-chat-list/:userId",getchatList);
 chatRouter.post("/markRead/:userId/:groupId?",markMessagesAsRead);
 chatRouter.post("/upload", uploadFile, (req, res) => {
-  if (!req.file) return res.status(400).json({ error: "No file uploaded" });
+  if (!req.file) return res.status(400).json({ error: "No file uploaded yet" });
 
   const fileUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
   res.status(200).json({ fileUrl });
