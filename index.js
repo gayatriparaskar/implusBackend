f66d535aa74846a71c4744a40daf794682884eb4
ZahidTadvi <92442854+ZahidTadvi@users.noreply.github.com>
2025-07-07 17:16:45 +0530

    making chnages for server to run on aws

--- index.js
+++ index.js
@@ -59,6 +59,10 @@ socketHandler(io);
 callSocketHandler(io);
 
 
-server.listen(5000, () => {
+// server.listen(5000, () => {
+//   console.log('Server running on port 5000');
+// });
+
+server.listen(5000, '0.0.0.0', () => {
   console.log('Server running on port 5000');
 });
